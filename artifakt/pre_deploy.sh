bfed1c1fe08ccbf839d7db21f68dd434193de82e
Antoine Kociuba <antoine.kociuba@artifakt.io>
2021-06-07 16:03:45 +0200

    pre_deploy.sh script

--- artifakt/pre_deploy.sh
+++ artifakt/pre_deploy.sh
@@ -10,9 +10,15 @@ else
     COMPOSER_MEMORY_LIMIT=-1 composer install --ansi
 fi
 
+# Check requirements before everything else
+php bin/console sylius:install:check-requirements
+
 # Installing project/DB (@see https://docs.sylius.com/en/1.9/cookbook/configuration/installation-commands.html)
 if [[ "$IS_INSTALLED" == "false" ]]; then
-    php bin/console sylius:install -e $ARTIFAKT_ENVIRONMENT_CRITICALITY -n --ansi
+    yes Y | php bin/console sylius:install:database -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
+    yes Y | php bin/console sylius:install:sample-data -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
+    php bin/console sylius:install:setup -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
+    php bin/console sylius:install:assets -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
 fi
 
 # Installing assets
