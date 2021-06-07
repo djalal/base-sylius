5e4867e65f93251a00657164823540ce3e36dfc2
Antoine Kociuba <antoine.kociuba@artifakt.io>
2021-06-07 17:37:11 +0200

    pre_deploy.sh script

--- artifakt/pre_deploy.sh
+++ artifakt/pre_deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Remove 'main instance ready' flag
+rm /mnt/shared/main_instance_ready.flag
+
 # @see https://github.com/Sylius/Sylius/issues/12685
 git apply patches/fix_doctrine_config.patch
 
-# Composer
+# Composer install
 if [[ "$ARTIFAKT_ENVIRONMENT_CRITICALITY" == "prod" ]]; then
     COMPOSER_MEMORY_LIMIT=-1 composer install --ansi --no-dev
 else
@@ -13,21 +16,28 @@ fi
 # Check requirements before everything else
 php bin/console sylius:install:check-requirements
 
-if [[ "$IS_INSTALLED" == "false" ]]; then
-    # Installing project/DB (@see https://docs.sylius.com/en/1.9/cookbook/configuration/installation-commands.html)
-    yes Y | php bin/console sylius:install:database -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
-    yes Y | php bin/console sylius:install:sample-data -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
-    php bin/console sylius:install:setup -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
-    php bin/console sylius:install:assets -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
+if [[ $ARTIFAKT_IS_MAIN_INSTANCE -eq 1 ]]; then
+    if [[ "$IS_INSTALLED" == "false" ]]; then
+        # Installing project/DB (@see https://docs.sylius.com/en/1.9/cookbook/configuration/installation-commands.html)
+        yes Y | php bin/console sylius:install:database -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
+        yes Y | php bin/console sylius:install:sample-data -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
+        php bin/console sylius:install:setup -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
+        php bin/console sylius:install:assets -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
+    else
+        # Build already installed project
+        php bin/console sylius:fixtures:load -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
+    fi
+
+    touch /mnt/shared/main_instance_ready.flag
 else
-    # Build already installed project
-    php bin/console sylius:fixtures:load -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
-    php bin/console sylius:install:assets -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
+    until [[ -f "/mnt/shared/main_instance_ready.flag" ]];
+    do sleep 10 && echo "Database is not up to date, waiting...";
+    done;
+    echo "Database is up to date.";
+    rm /mnt/shared/main_instance_ready.flag
 fi
 
-# Cache clear
-php bin/console cache:clear
-
 # Installing assets
+php bin/console sylius:install:assets -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
 yarn install
-yarn build
+GULP_ENV=$ARTIFAKT_ENVIRONMENT_CRITICALITY yarn build
