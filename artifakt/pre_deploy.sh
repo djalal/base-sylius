b81b923ababd6add31c0250ca399328768021105
Antoine Kociuba <antoine.kociuba@artifakt.io>
2021-06-08 20:58:40 +0200

    pre_deploy.sh script TMP

--- artifakt/pre_deploy.sh
+++ artifakt/pre_deploy.sh
@@ -6,7 +6,7 @@ rm /mnt/shared/main_instance_ready.flag
 # @see https://github.com/Sylius/Sylius/issues/12685
 git apply patches/fix_doctrine_config.patch
 
-# Composer install
+# Composer
 if [[ "$ARTIFAKT_ENVIRONMENT_CRITICALITY" == "prod" ]]; then
     COMPOSER_MEMORY_LIMIT=-1 composer install --ansi --no-dev
 else
@@ -18,26 +18,27 @@ php bin/console sylius:install:check-requirements
 
 if [[ $ARTIFAKT_IS_MAIN_INSTANCE -eq 1 ]]; then
     if [[ "$IS_INSTALLED" == "false" ]]; then
-        # Installing project/DB (@see https://docs.sylius.com/en/1.9/cookbook/configuration/installation-commands.html)
+        # Install fresh DB and project (@see https://docs.sylius.com/en/1.9/cookbook/configuration/installation-commands.html)
         yes Y | php bin/console sylius:install:database -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
         yes Y | php bin/console sylius:install:sample-data -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
         php bin/console sylius:install:setup -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
         php bin/console sylius:install:assets -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
     else
-        # Build already installed project
+        # Load fixtures
         php bin/console sylius:fixtures:load -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
     fi
 
     touch /mnt/shared/main_instance_ready.flag
 else
-    until [[ -f "/mnt/shared/main_instance_ready.flag" ]];
-    do sleep 10 && echo "Database is not up to date, waiting...";
-    done;
-    echo "Database is up to date.";
-    rm /mnt/shared/main_instance_ready.flag
+    # Wait until main instance is ready before carry on
+    #until [[ -f "/mnt/shared/main_instance_ready.flag" ]];
+    #do sleep 10 && echo "Database is not up to date, waiting...";
+    #done;
+    #echo "Database is up to date.";
+    #rm /mnt/shared/main_instance_ready.flag
 fi
 
-# Installing assets
+# Install/Build assets
 php bin/console sylius:install:assets -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
 yarn install
 GULP_ENV=$ARTIFAKT_ENVIRONMENT_CRITICALITY yarn build
