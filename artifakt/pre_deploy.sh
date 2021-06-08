8b206981777c2b8c9a4439f55e4ae4eaf92b7a59
Antoine Kociuba <antoine.kociuba@artifakt.io>
2021-06-09 00:24:53 +0200

    pre_deploy.sh script

--- artifakt/pre_deploy.sh
+++ artifakt/pre_deploy.sh
@@ -8,9 +8,9 @@ git apply patches/fix_doctrine_config.patch
 
 # Composer
 if [[ "$ARTIFAKT_ENVIRONMENT_CRITICALITY" == "prod" ]]; then
-    COMPOSER_MEMORY_LIMIT=-1 composer install --ansi --no-dev
+    COMPOSER_MEMORY_LIMIT=-1 composer install --ansi --no-interaction --no-dev
 else
-    COMPOSER_MEMORY_LIMIT=-1 composer install --ansi
+    COMPOSER_MEMORY_LIMIT=-1 composer install --ansi --no-interaction
 fi
 
 # Check requirements before everything else
@@ -19,10 +19,8 @@ php bin/console sylius:install:check-requirements
 if [[ $ARTIFAKT_IS_MAIN_INSTANCE -eq 1 ]]; then
     if [[ "$IS_INSTALLED" == "false" ]]; then
         # Install fresh DB and project (@see https://docs.sylius.com/en/1.9/cookbook/configuration/installation-commands.html)
-        yes Y | php bin/console sylius:install:database -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
-        yes Y | php bin/console sylius:install:sample-data -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
+        yes | php bin/console sylius:install:database -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi
         php bin/console sylius:install:setup -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
-        php bin/console sylius:install:assets -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
     else
         # Load fixtures
         php bin/console sylius:fixtures:load -e $ARTIFAKT_ENVIRONMENT_CRITICALITY --ansi -n
